0715cf734d0ea46454f40dd0dcb5f9e5f95e16fa
Alejandro Hugo Herms <hermsale@gmail.com>
2022-07-29 15:24:40 -0300

    curso practico de js - test

---
+++ Test de JavaScript/index.js
@@ -0,0 +1,7 @@
+// funcion declarativa 
+function persona(name,lastname,nickname){
+    let completeName = `Mi nombre es ${name} y mi apellido ${lastname}, tambien puedes decirme ${nickname}`;
+    console.log(completeName);
+}
+
+persona("Alejandro","Herms","HermsAle");
