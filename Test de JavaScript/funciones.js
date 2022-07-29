0cb718ab2a4c431aa7c0eaf103e95ccbfa982e76
Alejandro Hugo Herms <hermsale@gmail.com>
2022-07-29 16:07:52 -0300

    clase de listas, array y objetos

--- Test de JavaScript/funciones.js
+++ Test de JavaScript/funciones.js
@@ -1,3 +1,5 @@
+// Test de JS - Curso Practico JS
+
 // funcion declarativa 
 function persona(name,lastname,nickname){
     let completeName = `Mi nombre es ${name} y mi apellido ${lastname}, tambien puedes decirme ${nickname}`;
---
+++ Test de JavaScript/listas.js
@@ -0,0 +1,16 @@
+// listas - arrays - objetos 
+
+// listas: es un conjunto de elementos 
+
+// array: es una lista de elementos - Pueden ser de distintos tipos de datos.
+const array = [1,"nombre",true,"perro"];
+
+// Objeto: es una lista de elementos - Pero cada elemento tiene un nombre clave 
+const obj = {
+    nombre: 'Alejandro',
+    edad : 31,
+};
+
+console.log(obj);
+// mostramos los nombre clave 
+console.log(Object.keys(obj));
