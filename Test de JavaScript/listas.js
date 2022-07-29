c4a3c2cad8dc7b974b0c2d32841cc097500517fb
Alejandro Hugo Herms <hermsale@gmail.com>
2022-07-29 17:12:06 -0300

    se trabajo en ejemplo de reemplazo de varios if con un solo objeto

--- Test de JavaScript/listas.js
+++ Test de JavaScript/listas.js
@@ -30,3 +30,35 @@ console.log(obj);
 // mostramos los nombre clave 
 // console.log(Object.keys(obj));
 
+// convertir objeto en un array - segun sus valores
+
+arrayObj = Object.values(obj);
+arrayObj.forEach(element => {
+    console.log(element)
+});
+
+/////// OBJETOS - acceder como array
+
+// Tambien podemos encontrar elementos en un objeto de la siguiente forma (como si fuera un array)
+const elementoObj = 'edad'; // guardamos un valor en la variable
+console.log(obj[elementoObj]); // pasamos como si fuera un array con corchetes el elemento a buscar
+
+// OBJETOS - REDUCCION DE CONDICIONAL - EJEMPLO /////// 
+
+// Creamos un objeto con las opciones disponibles para una condicion - reemplazando a varios IF 
+const tiposDeSuscripcion ={
+    free:'Podes acceder a determinados cursos',
+    basic:'Podes acceder a contenido pago',
+    expert:'Podes acceder a contenido pago y una cartilla de ofertas laborales'
+}
+
+function conseguirTipoSuscripcion(suscripcion){
+    if(tiposDeSuscripcion[suscripcion]){
+        console.log(tiposDeSuscripcion[suscripcion]);
+        return;
+    }
+
+    console.warn('Ese tipo de suscripcion no existe');
+}
+
+conseguirTipoSuscripcion('asdsadsa');
